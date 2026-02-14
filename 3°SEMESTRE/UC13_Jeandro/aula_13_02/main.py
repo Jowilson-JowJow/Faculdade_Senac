fd73a4ae5011945b4a8a4329cf7239916b5cf97b
Jowilson-JowJow <captares@gmail.com>
2026-02-13 22:06:52 -0400

    aula

---
+++ 3°SEMESTRE/UC13_Jeandro/aula_13_02/main.py
@@ -0,0 +1,47 @@
+import tkinter as tk #tras os componentes para desenhar a tela no python
+
+numero = 0
+
+def lerNome():
+    global numero
+    numero +=1
+    labelResult.config(text=f'{numero}') # escrevendo em label
+    inputNome.delete(0, tk.END) #escrevendo no inputtext
+    inputNome.insert(0, numero)
+
+
+
+
+janela = tk.Tk()#criação da janela principal
+janela.title("Minha janela principal")# titulo da janela
+janela.geometry("600x400")
+
+labelNome = tk.Label(janela, text="Contador")
+labelNome.pack()
+
+inputNome = tk.Entry(janela)
+inputNome.pack()
+
+labelResult = tk.Label(janela, text="Resultado")
+labelResult.pack()
+
+botao = tk.Button(janela, text="ler", command=lerNome)
+botao.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+janela.mainloop()#looping que mantem a janela aberta -- sempre a ultima linha do codigo'

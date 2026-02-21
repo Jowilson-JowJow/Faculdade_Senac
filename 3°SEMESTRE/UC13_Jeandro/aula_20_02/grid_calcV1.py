1f911515c78149ae7d95ef31baadac32d244a5c7
Jowilson-JowJow <captares@gmail.com>
2026-02-21 15:41:30 -0400

    ajustes na calculadora

--- 3°SEMESTRE/UC13_Jeandro/aula_20_02/grid_calcV1.py
+++ 3°SEMESTRE/UC13_Jeandro/aula_20_02/grid_calcV1.py
@@ -39,19 +39,19 @@ janela.title("Calculadora by JowJow")
 janela.geometry("350x200")
 
 labelNum1 = tk.Label(janela, text="Numero 1: ")
-labelNum1.grid(row=0, column=0, padx=5, pady=5)
+labelNum1.grid(row=0, column=0, columnspan=2, padx=5, pady=10, sticky="e") # sticky "e" (east/direita)
 #labelNum1 = tk.Label(janela, text="Numero 1:").grid(row=0,column=0) -- codigo errado
 
 entrada1 = tk.Entry(janela)
-entrada1.grid(row=0, column=1, columnspan=3, padx=5, pady=5, sticky="ew")
+entrada1.grid(row=0, column=2, columnspan=2, padx=5, pady=10, sticky="w") # sticky "w" (west/esquerda)
 # entrada1 = tk.Entry(janela).grid(row=0,column=1) -- codigo errado
 
 labelNum2 = tk.Label(janela, text="Numero 2: ")
-labelNum2.grid(row=1, column=0, padx=5, pady=5)
+labelNum2.grid(row=1, column=0, columnspan=2, padx=5, pady=10, sticky="e")# sticky "e" (east/direita)
 # labelNum2 = tk.Label(janela, text="Numero 2:").grid(row=1,column=0)  -- codigo errado
 
 entrada2 = tk.Entry(janela)
-entrada2.grid(row=1, column=1, columnspan=3, padx=5, pady=5, sticky="ew")
+entrada2.grid(row=1, column=2, columnspan=2, padx=5, pady=10, sticky="w")
 # entrada2 = tk.Entry(janela).grid(row=1,column=1) -- codigo errado
 
 botao_Soma = tk.Button(janela, text="Somar", width=10, command=somar).grid(row=2, column=0, padx=3, pady=10)
@@ -59,8 +59,9 @@ botao_Subtrair = tk.Button(janela, text="Subtrair", width=10, command=subtrair).
 botao_mult = tk.Button(janela, text="Multiplicar", width=10, command=multiplicar).grid(row=2, column=2, padx=3)
 botao_div = tk.Button(janela, text="Dividir", width=10, command=dividir).grid(row=2, column=3, padx=3)
 
-labelResult = tk.Label(janela, text ="Resposta: ")
-labelResult.grid(row=3, column=0, columnspan=4, pady=10)
+labelResult = tk.Label(janela, text="Resposta:", font=("Arial", 10, "bold"))
+labelResult.grid(row=3, column=0, pady=10)
 # labelResult = tk.Label(janela, text="Resultado: ").grid(row=3,column=0) --codigo errado
 
 janela.mainloop()#esta linha é que deixa a janela aberta sem essa linha a janela executa e fecha - esta é ultima linha do codigo
+
